0c8328e971bce73b092ce26963eb30b2f0759077
Alexandre WENG <alexandre.weng@epita.fr>
2023-05-30 03:15:59 +0200

    wip: added prototype for eulerian_path O(E)? will be used to find a path visiting all edges instead of real CPP solution O(n^3) Hierholzer approximation O(E^2) Christofides approximation O(E^2log(V))

--- src/gui/graph_manip.py
+++ src/gui/graph_manip.py
@@ -53,6 +53,46 @@ def shortest_cpp_path(graph):
     
     return passing_order, cost
 
+def eulerian_path(G):
+    """
+    G: networkx.graph
+    Returrns the list of node representing the eulerian cycle in G
+
+    #pseudo-code not doc
+    eulerize G
+    find a eulerian path in the eulerized graph
+    create a valid edge path containing existing edges in G:
+        check the path if an edge existed in G no problem
+        if it did not: replace with the shortest path from source to destination of this
+        edge that didn't exist using djikstra
+    convert to a node list for coloring and consistency
+    """
+    # DON'T ASK ME IF THE CODE IS ALREADY GOOD, IT ISN'T
+    # THESE ARE JUST GENERAL INSTRUCTIONS TO IMPLEMENT
+    # DON'T FOLLOW IF IT DOSN'T WORK FOR YOU
+    # EACH LINE OF CODE IS TO BE TAKEN AS 'DO SOMETHING RESSEMBLING THAT' NOT 'DO THAT'
+    # eulerized = nx.eulerize() # eulerized will add edges to make the graph eulerian
+    # eulerian_path = nx.eulerian_path(eulerized) # O(E) apparently, find a path to visit all edges
+    # caution this eulerian path contains edge that didn't exist in the original G graph
+    # iterate over the path and verify that the edge already existed in the originial G
+    # if it did OK, if it didn't change it using djikstra
+    # for src, dst in eulerian_path: # maybe iterate with an i and range(len(eulerian_path))
+    # try: 
+    #   G[src][dst] # good no problem
+    # except: 
+    #   try:
+    #       G[dst][src]  # good no problem
+    #   except:
+    #       change this src -> dst in eulerian_path to nx.dijkstra_path(G, src, dst)
+    # DON'T ASK ABOUT THE REPRESENTATION, 1->3 ISN'T HOW IT IS ACTUALLY CODED
+    # example eulerian path is [(1->3),(3->4),(4->2)] but 3->4 didn't exist in the original graph
+    # assuming 3->1->4 is a valid path and the shortest from 3 to 4 that actually exists
+    # change the path to [(1->3),(3->1),(1->4),(4->2)]
+    # NOW change this edge path to a node path
+    # [1,3,1,4,2]
+    # FIXME
+    pass
+
 def evaluate_solution(cost, time, weights):
     """
     returns the effective cost according to the weigths given for cost and time
