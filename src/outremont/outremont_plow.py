a376722118150c901923d3d25bfacad6284f4bd0
Alexandre WENG <alexandre.weng@epita.fr>
2023-06-04 11:29:35 +0200

    refactor(outremont_plow.py): cleanup refactor TODO copy to the other subfolders

--- src/outremont/outremont_plow.py
+++ src/outremont/outremont_plow.py
@@ -1,49 +1,55 @@
 from argparse import ArgumentParser
-from pathlib import Path
+import argparse
+import osmnx as ox
 import pickle
 import sys
-sys.path.append('..')
-from opti import opti
-import matplotlib.pyplot as plt
-import numpy as np
-from utils import *
-import osmnx as ox
-from graph_manip import eulerian_path
 
-try:
-    budget = 1000 #valeur par défaut
-    time = 2 #valeur par défaut
-    vehicle = 20 #valeur par défaut
+sys.path.append('..')
+from graph_manip import eulerian_path
+from opti import opti
+from utils import pickle_load, display_graph
+
+AREA="Outremont"
+MONTREAL_QC = "Montreal, QC, Canada"
+FULL_AREA = f"{AREA}, {MONTREAL_QC}"
+OSMNX_NETWORK_TYPE = "drive"
+DEFAULT_BUDGET = 1000
+DEFAULT_TIME = 2
+DEFAULT_VEHICLE = 20
+
+if __name__ == '__main__':
+    budget = DEFAULT_BUDGET
+    time = DEFAULT_TIME
+    vehicle = DEFAULT_VEHICLE
     load = False
-    
+
     #Recup les arguments
     parser = ArgumentParser()
-    parser.add_argument("--load", required=False, type=str)
-    parser.add_argument("--budget", required=False, type=str)
-    parser.add_argument("--time", required=False, type=str)
-    parser.add_argument("--vehicle", required=False, type=str)
+    parser.add_argument("--load", '-l', action=argparse.BooleanOptionalAction)
+    parser.add_argument("--budget", '-b', required=False, type=str)
+    parser.add_argument("--time", '-t', required=False, type=str)
+    parser.add_argument("--vehicle", '-n', required=False, type=str)
     args = parser.parse_args()
-    
 
-    if args.load != "" :     
-        load = args.load == "True"
-    if args.budget != "" : 
+    load = args.load
+    if args.budget: 
         budget = int(args.budget)
-    if args.time != "" :
+    if args.time:
         time = int(args.time)
-    if args.vehicle != "" :
+    if args.vehicle:
         vehicle = int(args.vehicle)
-    
-    if  (not load):
-        network = ox.graph_from_place("Outremont, Montreal, QC, Canada", network_type="drive")
+
+    if not load:
+        network = ox.graph_from_place(FULL_AREA, network_type=OSMNX_NETWORK_TYPE)
         eulerized, path = eulerian_path(network.to_undirected())
-        pickle.dump(path, open("../../Outremont-drone.p", "wb"))
-        pickle.dump(eulerized, open("../../Outremont-eulerized.p", "wb"))
+        pickle.dump(path, open(f"{AREA}-path.p", "wb"))
+        pickle.dump(eulerized, open(f"{AREA}-eulerized.p", "wb"))
     else :
-        eulerized= pickle_load("../../Outremont-eulerized.p")
-        path = pickle_load("../../Outremont-drone.p")
-    
+        try:
+            eulerized= pickle_load(f"{AREA}-eulerized.p")
+            path = pickle_load(f"{AREA}-path.p")
+        except FileNotFoundError:
+            print("Générer le fichier avant de lancer l'étude")
+
     graph = [opti(time, budget, eulerized, path, n) for n in range(1,vehicle + 1)]
-    display_graph(graph, str(budget), str(time), "Outremont", vehicle + 1)
-except FileNotFoundError:
-    print("Générer le fichier avant de lancer l'étude")
+    display_graph(graph, str(budget), str(time), f"{AREA}", vehicle + 1)
